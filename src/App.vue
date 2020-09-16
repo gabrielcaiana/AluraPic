853b7722f2e2e0d9b0e1f0625868d3058822be97
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-09-16 01:43:54 -0300

    estilizando cards de imagens

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <ul class="grid-images">
       <li v-for="(foto, index) of fotos" :key="index">
         <img :src="foto.url" :alt="foto.titulo" />
+        <h3>{{foto.titulo}}</h3>
       </li>
     </ul>
   </div>
@@ -28,12 +29,15 @@ export default {
 <style scoped>
   .corpo {
     width: 100%;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     font-family: Helvetica, sans-serif;
     color: gray;
+    background-color: #18171E;
+    box-sizing: border-box;
   }
   .grid-images {
     display: grid;
@@ -42,15 +46,24 @@ export default {
     align-items: center;
     gap: 20px;
     padding: 0;
+    margin-top: 32px;
   }
   .grid-images li {
     list-style: none;
+    height: 400px;
+    background-color: #39364b;
+    border-radius: 5px;
   }
   .grid-images li img {
     width: 300px;
-    height: 300px;
+    height: 80%;
     object-fit: cover;
-    border-radius: 4px;
+    border-radius: 4px 4px 0 0 ;
+  }
+  .grid-images li h3 {
+    margin-top: 16px;
+    text-align: center;
+    color: #fff;
   }
 </style>
 
