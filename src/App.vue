48bb386ad8011ec342e787b55479565e3718f5a0
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-09-16 01:19:54 -0300

    estilizando o componente

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="corpo">
     <h1>{{ titulo }}</h1>
-    <ul>
+    <ul class="grid-images">
       <li v-for="(foto, index) of fotos" :key="index">
         <img :src="foto.url" :alt="foto.titulo" />
       </li>
@@ -25,4 +25,32 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+  .corpo {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: Helvetica, sans-serif;
+    color: gray;
+  }
+  .grid-images {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    padding: 0;
+  }
+  .grid-images li {
+    list-style: none;
+  }
+  .grid-images li img {
+    width: 300px;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 4px;
+  }
+</style>
+
